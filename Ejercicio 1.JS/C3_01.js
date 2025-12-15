5e214cb22dc23ce053c60ab4a507ce613210a413
J0anGab10 <juang.ch100@gmail.com>
2025-12-15 14:24:04 +0100

    C3_01 sin comentarios

--- Ejercicio 1.JS/C3_01.js
+++ Ejercicio 1.JS/C3_01.js
@@ -1,8 +1,15 @@
 //--3. Función para calcular un descuento--
 
+function Descuento(precio, descuento) {
+    return precio = precio * descuento; 
+}
 let precio = prompt("Calculo de un descuento.\n\n¿Introduce el precio del producto?");
 let descuento = prompt("Calculo de un descuento.\n\n¿Introduce el numero sin porcentaje del descuento?");
 
 precio = Number(precio);
 descuento = Number(descuento);
+
+descuento = descuento/100;
+
+console.log(Descuento(precio,descuento));
 
